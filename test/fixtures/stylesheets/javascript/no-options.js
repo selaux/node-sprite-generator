b092125d28a60da97cbb96c8d91a04446569711e
siemiatj <kuba@saucelabs.com>
2017-02-15 19:03:51 +0100

    - create test cases

---
+++ test/fixtures/stylesheets/javascript/no-options.js
@@ -0,0 +1,11 @@
+var SPRITES = {
+
+  FOO: { x: 0, y: 0, width: 150, height: 12 },
+
+  BAR: { x: 0, y: 12, width: 150, height: 24 },
+
+  TEST: { x: 0, y: 36, width: 150, height: 12 },
+
+};
+
+module.exports = SPRITES;
---
+++ test/fixtures/stylesheets/javascript/with-nameMapping.js
@@ -0,0 +1,11 @@
+var SPRITES = {
+
+  OOF: { x: 0, y: 0, width: 150, height: 12 },
+
+  RAB: { x: 0, y: 12, width: 150, height: 24 },
+
+  TSET: { x: 0, y: 36, width: 150, height: 12 },
+
+};
+
+module.exports = SPRITES;
