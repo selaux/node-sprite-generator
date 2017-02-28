0cb8db57c1a4757cbb0abac5b2e3178c2a8bbdff
Stefan Lau <github@stefanlau.com>
2017-02-28 21:57:07 +0100

    Remove default options from layouts

--- test/specs/layout/packed.js
+++ test/specs/layout/packed.js
@@ -11,29 +11,10 @@ describe('Layout/Packed', function () {
         { path: 'bla', width: 10, height: 10, data: 'image3' }
     ];
 
-    it('should generate the correct layout without any options', function () {
-        var options = {};
-
-        return packed(images, options).then(function (layout) {
-            expect(options).to.deep.equal({});
-            expect(layout).to.deep.equal({
-                width: 30,
-                height: 20,
-                images: [
-                    _({ x: 0, y: 0 }).extend(images[0]),
-                    _({ x: 20, y: 0 }).extend(images[1]),
-                    _({ x: 20, y: 10 }).extend(images[2])
-                ]
-            });
-            expect(layout.images[0]).not.to.equal(images[0]);
-        });
-    });
-
     it('should generate the correct layout when a padding is specified', function () {
-        var options = { padding: 10 };
+        var options = { scaling: 1, padding: 10 };
 
         return packed(images, options).then(function (layout) {
-            expect(options).to.deep.equal({ padding: 10 });
             expect(layout).to.deep.equal({
                 width: 50,
                 height: 50,
@@ -48,10 +29,9 @@ describe('Layout/Packed', function () {
     });
 
     it('should generate the correct layout when a scaling is specified', function () {
-        var options = { scaling: 0.5 };
+        var options = { scaling: 0.5, padding: 0 };
 
         return packed(images, options).then(function (layout) {
-            expect(options).to.deep.equal({ scaling: 0.5 });
             expect(layout).to.deep.equal({
                 width: 15,
                 height: 10,
--- test/specs/layout/vertical.js
+++ test/specs/layout/vertical.js
@@ -11,29 +11,10 @@ describe('Layout/Vertical', function () {
         { path: 'bla', width: 112, height: 112 }
     ];
 
-    it('should generate the correct layout without any options', function () {
-        var options = {};
-
-        return vertical(images, {}).then(function (layout) {
-            expect(options).to.deep.equal({});
-            expect(layout).to.deep.equal({
-                width: 150,
-                height: 156,
-                images: [
-                    _({ x: 0, y: 0 }).extend(images[0]),
-                    _({ x: 0, y: 12 }).extend(images[1]),
-                    _({ x: 0, y: 44 }).extend(images[2])
-                ]
-            });
-            expect(layout.images[0]).not.to.equal(images[0]);
-        });
-    });
-
     it('should generate the correct layout when a padding is specified', function () {
-        var options = { padding: 50 };
+        var options = { scaling: 1, padding: 50 };
 
-        return vertical(images, { padding: 50 }).then(function (layout) {
-            expect(options).to.deep.equal({ padding: 50 });
+        return vertical(images, options).then(function (layout) {
             expect(layout).to.deep.equal({
                 width: 150,
                 height: 256,
@@ -48,10 +29,9 @@ describe('Layout/Vertical', function () {
     });
 
     it('should generate the correct layout when a scaling is specified', function () {
-        var options = { scaling: 0.7 };
+        var options = { scaling: 0.7, padding: 0 };
 
         return vertical(images, options).then(function (layout) {
-            expect(options).to.deep.equal({ scaling: 0.7 });
             expect(layout).to.deep.equal({
                 width: 105,
                 height: 108,
