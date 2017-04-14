aaa298f7f0fb2df62af3afb85ea27255dfd0e723
Stefan Lau <github@stefanlau.com>
2017-04-14 13:29:38 +0200

    Dependency inject and refactor change detector

--- test/unit/utils/changeDetector.js
+++ test/unit/utils/changeDetector.js
@@ -1,37 +1,43 @@
 'use strict';
 
-var proxyquire = require('proxyquire'),
-    sinon = require('sinon'),
-    expect = require('chai').expect,
-    inNode = require('../../utils/platform').inNode;
+const R = require('ramda');
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+const createChangeDetector = require('../../../lib/utils/changeDetector');
 
-inNode(describe, 'Utils/ChangeDetector', function () {
-    var options = {
+chai.use(require('chai-as-promised'));
+
+describe('Utils/ChangeDetector', function () {
+    const defaultPath = 'test/fixtures/images/src/*.png';
+    const options = {
         src: [
-            'test/fixtures/images/src/*.png'
-        ],
-        stylesheetPath: 'test/fixtures/stylesheet.styl',
-        spritePath: 'test/fixtures/sprite.png'
+            defaultPath
+        ]
     };
 
+    function buildDefaultDependencies() {
+        return {
+            glob: sinon.stub().resolves(),
+            stat: sinon.stub().rejects()
+        };
+    }
+
     it('should always return true the first time detect is called', function () {
-        var stubs = { glob: sinon.stub.yieldsAsync(null, []) },
-            createChangeDetector = proxyquire('../../../lib/utils/changeDetector', stubs),
-            detector = createChangeDetector(options);
+        const dependencies = buildDefaultDependencies();
+        const detector = createChangeDetector(dependencies, options);
 
         return expect(detector.detect()).to.eventually.be.true;
     });
 
     it('should return false if nothing changed', function () {
-        var stubs = {
-                glob: sinon.stub().yieldsAsync(null, [ 'myfile', 'myfile2' ]),
-                fs: { stat: sinon.stub() }
-            },
-            createChangeDetector = proxyquire('../../../lib/utils/changeDetector', stubs),
-            detector = createChangeDetector(options);
+        const dependencies = buildDefaultDependencies();
 
-        stubs.fs.stat.withArgs('myfile').yieldsAsync(null, { mtime: new Date(0) });
-        stubs.fs.stat.withArgs('myfile2').yieldsAsync(null, { mtime: new Date(10) });
+        dependencies.glob.withArgs(defaultPath).resolves([ 'myfile', 'myfile2' ]);
+        dependencies.stat.withArgs('myfile').resolves({ mtime: new Date(0) });
+        dependencies.stat.withArgs('myfile2').resolves({ mtime: new Date(10) });
+
+        const detector = createChangeDetector(dependencies, options);
 
         return expect(detector.register().then(function() {
             return detector.detect();
@@ -39,16 +45,14 @@ inNode(describe, 'Utils/ChangeDetector', function () {
     });
 
     it('should return true if any of the files have changed', function () {
-        var stubs = {
-                glob: sinon.stub().yieldsAsync(null, [ 'myfile', 'myfile2' ]),
-                fs: { stat: sinon.stub() }
-            },
-            createChangeDetector = proxyquire('../../../lib/utils/changeDetector', stubs),
-            detector = createChangeDetector(options);
+        const dependencies = buildDefaultDependencies();
 
-        stubs.fs.stat.withArgs('myfile').onFirstCall().yieldsAsync(null, { mtime: new Date(0) });
-        stubs.fs.stat.withArgs('myfile').onSecondCall().yieldsAsync(null, { mtime: new Date(10) });
-        stubs.fs.stat.withArgs('myfile2').yieldsAsync(null, { mtime: new Date(10) });
+        dependencies.glob.withArgs(defaultPath).resolves([ 'myfile', 'myfile2' ]);
+        dependencies.stat.withArgs('myfile').resolves({ mtime: new Date(0) });
+        dependencies.stat.withArgs('myfile2').onFirstCall().resolves({ mtime: new Date(10) });
+        dependencies.stat.withArgs('myfile2').onSecondCall().resolves({ mtime: new Date(20) });
+
+        const detector = createChangeDetector(dependencies, options);
 
         return expect(detector.register().then(function() {
             return detector.detect();
@@ -56,17 +60,48 @@ inNode(describe, 'Utils/ChangeDetector', function () {
     });
 
     it('should return true if any of the files are deleted', function () {
-        var stubs = {
-                glob: sinon.stub().yieldsAsync(null, [ 'myfile', 'myfile2' ]),
-                fs: { stat: sinon.stub() }
-            },
-            createChangeDetector = proxyquire('../../../lib/utils/changeDetector', stubs),
-            detector = createChangeDetector(options),
-            error = { code: 'ENOENT' };
+        const dependencies = buildDefaultDependencies();
 
-        stubs.fs.stat.withArgs('myfile').yieldsAsync(null, { mtime: new Date(10) });
-        stubs.fs.stat.withArgs('myfile2').onFirstCall().yieldsAsync(null, { mtime: new Date(0) });
-        stubs.fs.stat.withArgs('myfile2').onSecondCall().yieldsAsync(error);
+        dependencies.glob.withArgs(defaultPath).resolves([ 'myfile3', 'myfile4' ]);
+        dependencies.stat.withArgs('myfile3').resolves({ mtime: new Date(0) });
+        dependencies.stat.withArgs('myfile4').onFirstCall().resolves({ mtime: new Date(10) });
+        dependencies.stat.withArgs('myfile4').onSecondCall().rejects({ code: 'ENOENT' });
+
+        const detector = createChangeDetector(dependencies, options);
+
+        return expect(detector.register().then(function() {
+            return detector.detect();
+        })).to.eventually.be.true;
+    });
+
+    it('should return detect when stylesheet is changed', function () {
+        const stylesheetPath = 'stylesheet';
+        const dependencies = buildDefaultDependencies();
+
+        dependencies.glob.withArgs(stylesheetPath).resolves([ 'mystylesheetfile' ]);
+        dependencies.glob.withArgs(defaultPath).resolves([ 'myOtherfile' ]);
+        dependencies.stat.withArgs('myOtherfile').resolves({ mtime: new Date(0) });
+        dependencies.stat.withArgs('mystylesheetfile').onFirstCall().resolves({ mtime: new Date(10) });
+        dependencies.stat.withArgs('mystylesheetfile').onSecondCall().resolves({ mtime: new Date(100) });
+
+        const detector = createChangeDetector(dependencies, R.merge(options, { stylesheetPath }));
+
+        return expect(detector.register().then(function() {
+            return detector.detect();
+        })).to.eventually.be.true;
+    });
+
+    it('should return detect when stylesheet is changed', function () {
+        const spritePath = 'sprite';
+        const dependencies = buildDefaultDependencies();
+
+        dependencies.glob.withArgs(spritePath).resolves([ 'myspritefile' ]);
+        dependencies.glob.withArgs(defaultPath).resolves([ 'myOtherfile' ]);
+        dependencies.stat.withArgs('myOtherfile').resolves({ mtime: new Date(0) });
+        dependencies.stat.withArgs('myspritefile').onFirstCall().resolves({ mtime: new Date(10) });
+        dependencies.stat.withArgs('myspritefile').onSecondCall().resolves({ mtime: new Date(100) });
+
+        const detector = createChangeDetector(dependencies, R.merge(options, { spritePath }));
 
         return expect(detector.register().then(function() {
             return detector.detect();
